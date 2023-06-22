c8f725eb17ff21144550f5c35d262f8af9f39e8e
vikram1311s <71538224+vikram1311s@users.noreply.github.com>
2023-06-22 07:46:17 +0530

    Calculator With Function - (multi missing)

--- Training/react-app/src/Calculator.js
+++ Training/react-app/src/Calculator.js
@@ -1,20 +1,51 @@
 import './Calculator.css';
+import {useState} from 'react'
 
 function Calculator() {
+
+    const [output, setOutput] = useState('');
+    const [operator, setOperator] = useState(null);
+
+
+    const handleArthematic = (type) => {
+        if(type == 'C'){
+            setOutput('');
+            return;
+        }
+        let o = output;
+        o += type;
+        setOutput(o)
+        setOperator(type)
+    }
+
+    const handleResult = () => {
+       const splitString = output.split(operator);
+       if(operator == '+'){
+        const finalOutput = parseInt(splitString[0]) + parseInt(splitString[1]) 
+        setOutput(finalOutput);
+       } else if (operator == '-'){
+        const finalOutput = parseInt(splitString[0]) - parseInt(splitString[1]) 
+        setOutput(finalOutput);
+       }
+
+       console.log(splitString);
+    }
+
     return <div className="main-calc">
-        <input type="text" name="calc" className="input-cls" />
+        <input type="text" name="calc" value={output} onChange={(e) => setOutput(e.target.value)} className="input-cls" />
+
         <div className="operations-block">
             <div className="left">
                 <div className="child-left">
-                    <div className="operation">C</div>
-                    <div className="operation">+</div>
+                    <div className="operation" onClick={() => handleArthematic('C')}>C</div>
+                    <div className="operation" onClick={() => handleArthematic('+')}>+</div>
                     <div className="operation">*</div>
-                    <div className="operation">-</div>
+                    <div className="operation" onClick={() => handleArthematic('-')}>-</div>
                 </div>
 
             </div>
             <div className="right">
-                <div className="equal">=</div>
+                <div className="equal" onClick={() => handleResult()}>=</div>
             </div>
         </div>
 
