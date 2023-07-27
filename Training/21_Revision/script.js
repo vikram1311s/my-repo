7b4476aebd6d40963e8deee3b0029c07c53288d6
vikram1311s <71538224+vikram1311s@users.noreply.github.com>
2023-07-27 07:57:21 +0530

    revision react

--- Training/21_Revision/script.js
+++ Training/21_Revision/script.js
@@ -18,4 +18,66 @@ var person2 = person
 person2.name = 'ramesh'
 
 
-var persons = [1,2,4,5,7]
+var numbers = [1,2,4,5,7];
+console.log(numbers[0]);
+
+let numbers = [1, 2, 3, 4, 5, 6]
+
+let arrOfObjects = [{
+    name: 'suresh',
+    age: 64
+  },
+  {
+    name: 'ramesh',
+    age: 34
+  }
+]
+
+arrOfObjects = arrOfObjects.map((person) => {
+  if(person.age > 60){
+    person.seniorCitizen = true;
+  } else {
+    person.seniorCitizen = false;
+  }
+  return person;
+})
+
+console.log(arrOfObjects)
+
+function printName(name){
+
+    return 'Hi '+ name
+ }
+ 
+ let x = printName('test')
+ 
+ console.log(x)
+
+ fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => console.log(json))
+            
+
+
+  var x  = 10;
+
+  setTimeout(() => {
+     console.log('test')
+  }, 1000);
+
+  console.log(x)
+
+
+  var x  = 10;
+
+   fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+  console.log(x)
+
+
+
+
+
+
